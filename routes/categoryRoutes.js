658576c2f2637007402166775ac18f251c9afad6
HarshSaini3878 <ha1903330@gmail.com>
2024-06-27 00:00:49 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { IsAdmin, requireSignIn } from '../middlewares/auth.middleware';
-import {createCategoryController} from './../controllers/CategoryController'
+import {categoryControlller, createCategoryController, deleteCategoryCOntroller, singleCategoryController, updateCategoryController} from './../controllers/CategoryController'
 const router=expres();
 
-router.post('create-category',requireSignIn,IsAdmin,createCategoryController)
+router.post('/create-category',requireSignIn,IsAdmin,createCategoryController);
+router.put('/update-category/:id',requireSignIn,IsAdmin,updateCategoryController)
+router.get('/get-category',categoryControlller);
+router.get('/single-category/:slug',singleCategoryController);
+router.delete('/delete-category/:id',requireSignIn,IsAdmin,deleteCategoryCOntroller)
 export default router;
